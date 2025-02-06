e1bf74ee9a0409c31b4fe4fcaf42da73ecf30691
Егор <94907145+ShvetsovEgor@users.noreply.github.com>
2025-02-06 14:48:59 +0300

    json parser

---
+++ parser/main.py
@@ -0,0 +1,168 @@
+import json
+import time
+
+import requests
+from PyPDF2 import PdfReader
+from bs4 import BeautifulSoup
+
+
+def from_title_to_filename(title):
+    return "".join([x for x in title if x.isalpha() or x.isdigit()]) + '.pdf'
+
+
+def pdf_to_json(filename):
+    reader = PdfReader(filename)
+    number_of_pages = len(reader.pages)
+    text = ''
+    for i in range(number_of_pages):
+        page = reader.pages[i]
+        text += page.extract_text()
+    # print('Abstract' in text)
+    # print('Introduction' in text)
+    # print('References' in text)
+    # text = text.replace('Abstract', 'wbfjwfbjjefh')
+    # text = text.replace('Introduction', 'wbfjwfbjjefh')
+    # text = text.replace('References', 'wbfjwfbjjefh')
+    # text = text.split('wbfjwfbjjefh')
+    # print(len(text))
+    # abstract = text[1]
+    # full_text = text[2]
+
+    return text, text
+
+
+def extract_download_link(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    button_tag = soup.find('button', onclick=True)
+    if button_tag:
+        link = button_tag['onclick'].split("'")[1]
+        link = 'https:' + link
+
+        return link
+    return None
+
+
+def get_doi_year(title):
+    """Ищет DOI статьи по её названию через CrossRef API"""
+    url = f"https://api.crossref.org/works?query.title={title}&rows=1"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+
+        if "message" in data and "items" in data["message"] and data["message"]["items"]:
+            try:
+                year = str(data["message"]["items"][0].get('license')[0].get('start').get('date-parts')[0][0])
+            except Exception as e:
+                year = None
+            return data["message"]["items"][0].get("DOI"), year
+    return None, None
+
+
+def download_paper(doi, output_filename):
+    time.sleep(1)
+    sci_hub_url = "https://sci-hub.se"  # Рабочее зеркало может измениться
+    search_url = f"{sci_hub_url}/{doi}"
+
+    headers = {"User-Agent": "Mozilla/5.0"}  # Имитация браузера
+    session = requests.Session()
+
+    # Получаем HTML страницы
+    response = session.get(search_url, headers=headers)
+    if response.status_code != 200:
+        print("Ошибка доступа к Sci-Hub!")
+        print(sci_hub_url)
+        return
+
+    # Извлекаем ссылку на PDF
+    download_url = extract_download_link(response.text)
+    # print('not formated', download_url)
+
+    if download_url is None:
+        print('Cтатья отсутствует в sci hub')
+        return False
+    else:
+        download_url = download_url.replace('https:/downloads', 'https://sci-hub.ru/downloads')
+        download_url = download_url.replace('https:/tree', 'https://sci-hub.ru/tree')
+        print('formated', download_url)
+
+        pdf_response = session.get(download_url, headers=headers, stream=True)
+        if pdf_response.status_code == 200:
+            with open(output_filename, "wb") as f:
+                for chunk in pdf_response.iter_content(1024):
+                    f.write(chunk)
+            print(f"Файл сохранен как {output_filename}")
+        else:
+            print("Ошибка при загрузке PDF!")
+        return True
+
+
+# Функция для поиска статей
+def search_google_scholar(query, num_results=10):
+    pages = num_results // 10
+
+    # Находим все результаты
+    results = []
+
+    for i in range(pages):
+        time.sleep(5)
+        search_url = f'https://scholar.google.com/scholar?start={i * 10}&q={query}'
+        headers = {
+            'User-Agent': 'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+        }
+        # Выполнение запроса
+
+        response = requests.get(search_url, headers=headers)
+
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        for item in soup.find_all("h3"):  # Заголовки статей обычно находятся в <h3>
+            title = item.get_text()
+            title = ''.join([x for x in title if x.isalpha() or x == ' '])
+
+            link = item.a['href'] if item.a else None
+            results.append({
+                'title': title,
+                'link': link
+            })
+
+    return results
+
+
+def main(obj=None):
+    # searching at google scholar
+    query = ('smoking longevity interventions')
+    articles = search_google_scholar(query, num_results=20)
+
+    cnt = 0
+    papers = []
+    for a in articles:
+        title = a['title']
+
+        doi, year = get_doi_year(title)
+        # stroing papers locally
+        filename = from_title_to_filename(title)
+        if download_paper(doi, output_filename=f"{filename}.pdf"):
+            cnt += 1
+            papers += [(title, doi, year, filename)]
+
+    print(f"{cnt}/{len(articles)} papers are saved")
+    # forming json file
+    general = []
+    for p in papers:
+        title, doi, year, filename = p
+        abstract, fulltext = pdf_to_json(f"{title}.pdf")
+        dct = {
+            'title': title,
+            'year': year,
+            'doi': doi,
+            'abstract': abstract,
+            'fulltext': fulltext,
+            'filename': filename
+        }
+        general += [dct]
+    with open("data.json", "w") as file:
+        json.dump(general, file)
+
+
+if __name__ == "__main__":
+    main()
