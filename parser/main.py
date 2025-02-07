d04652b6e35f86adc149e0d93fa5351ae937866a
Егор <94907145+ShvetsovEgor@users.noreply.github.com>
2025-02-07 11:07:31 +0300

    Add files via upload

--- parser/main.py
+++ parser/main.py
@@ -8,33 +8,52 @@ from bs4 import BeautifulSoup
 from gigachat import GigaChat
 
 
-def extract_abstract(article_text: str) -> str:
+def extract_abstract(first_2_pages: str, filename: str) -> str:
+    with open(filename + '.txt', 'w') as f:
+        f.write(first_2_pages)
     model = GigaChat(
         credentials="YTUwN2I4ZTQtM2Q1ZC00NzY3LTkzYWQtOTI0MTlkMGQ3NGI4OmI3YzFkMTM4LTEyNTgtNDNkYS1hZTNkLTNiYmY4ZTc5NjFiMA==",
         scope="GIGACHAT_API_CORP", model="GigaChat-Pro",
         verify_ssl_certs=False, )
-    response = model.chat("Извлеки раздел 'Abstract' (аннотацию) из следующей статьи. "
-                          "Текст ответа должен состоять только из текста запроса. "
-                          "Ответ должен содержать только текст раздела 'Abstract', без дополнительных комментариев:\n\n"
-                          f"{article_text}")
-    return response.choices[0].message.content
+    file = model.upload_file(open(filename + '.txt', "rb"))
+    result = model.chat(
+        {
+            "messages": [
+                {
+                    "role": "user",
+                    "content": "Извлеки раздел 'Abstract' (аннотацию) из следующей статьи. "
+                               "Текст ответа должен состоять только из текста запроса. "
+                               "Ответ должен содержать только текст раздела 'Abstract', без дополнительных комментариев:\n\n",
+                    "attachments": [file.id_],
+                }
+            ],
+            "temperature": 0.1
+        }
+    )
+
+    return result.choices[0].message.content
 
 
 def from_title_to_filename(title):
-    return "".join([x for x in title if x.isalpha() or x.isdigit()]) + '.pdf'
+    return "".join([x for x in title if x.isalpha() or x.isdigit()])
 
 
 def pdf_to_json(filename):
-    reader = PdfReader(filename)
-    number_of_pages = len(reader.pages)
-    text = ''
-    for i in range(number_of_pages):
-        page = reader.pages[i]
-        text += page.extract_text()
+    try:
+        reader = PdfReader(filename)
+        number_of_pages = len(reader.pages)
+        text = ''
 
-    abstract = extract_abstract(text)
+        first_2_pages = reader.pages[0].extract_text() + reader.pages[1].extract_text()
+        for i in range(number_of_pages):
+            page = reader.pages[i]
+            text += page.extract_text()
 
-    return abstract, text
+        abstract = extract_abstract(first_2_pages, filename)
+
+        return abstract, text
+    except Exception as e:
+        return None, None
 
 
 def extract_download_link(html):
@@ -89,6 +108,7 @@ def download_paper(doi, output_filename):
     else:
         download_url = download_url.replace('https:/downloads', 'https://sci-hub.ru/downloads')
         download_url = download_url.replace('https:/tree', 'https://sci-hub.ru/tree')
+        download_url = download_url.replace('https:/uptodate', 'https://sci-hub.ru/uptodate')
         print('formated', download_url)
 
         pdf_response = session.get(download_url, headers=headers, stream=True)
@@ -134,11 +154,11 @@ def search_google_scholar(query, num_results=10):
     return results
 
 
-def main(obj=None):
+def main(obj='vitamin c'):
     # searching at google scholar
-    query = ('smoking longevity interventions')
-    articles = search_google_scholar(query, num_results=20)
-
+    query = (f'{obj} longevity effect')
+    articles = search_google_scholar(query, num_results=30)
+    print(articles)
     cnt = 0
     papers = []
     for a in articles:
@@ -156,7 +176,7 @@ def main(obj=None):
     general = []
     for p in papers:
         title, doi, year, filename = p
-        abstract, fulltext = pdf_to_json(f"{title}.pdf")
+        abstract, fulltext = pdf_to_json(f"{filename}.pdf")
         dct = {
             'title': title,
             'year': year,
